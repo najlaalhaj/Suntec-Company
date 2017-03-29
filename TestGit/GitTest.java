1494adc90ba1fabf1a2894d8da5b976155843ae7
user <user@172.16.1.71>
2017-03-29 14:43:51 +0200

    update GitTest.java

--- TestGit/GitTest.java
+++ TestGit/GitTest.java
@@ -3,5 +3,9 @@ public class GitTest {
 	
 	public void tester(){
 	System.out.println("Testing Git.");
+	public static void main(String args[]){
+		GitTest gittester = new GitTest();
+		System.out.println(gittester.toString());
+	}
 	}
 }
