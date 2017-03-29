d76e52a9a064a17888af13636b342925d72d735d
user <user@172.16.1.71>
2017-03-29 15:00:51 +0200

    Adding one more line!

--- TestGit/src/GitTest.java
+++ TestGit/src/GitTest.java
@@ -3,9 +3,11 @@ public class GitTest {
 	
 	public void tester(){
 	System.out.println("Testing Git.");
+	}
 	public static void main(String args[]){
 		GitTest gittester = new GitTest();
 		System.out.println(gittester.toString());
+		gittester.tester();
 	}
 	}
-}
+
